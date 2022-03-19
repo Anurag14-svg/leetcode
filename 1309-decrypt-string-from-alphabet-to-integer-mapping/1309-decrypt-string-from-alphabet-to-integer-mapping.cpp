8d5e0e13e93eac6a97ef880ef84631e5b758aa72
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-03-19 19:25:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (40.44%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -4,7 +4,7 @@ public:
         string res;
   for (int i = 0; i < s.size(); ++i) {
     if (i < s.size() - 2 && s[i + 2] == '#') {
-      res += 'j' + (s[i] - '1') * 10 + s[i + 1] - '0';
+      res += 'a' + (s[i] - '0') * 10 + s[i + 1] - '0' - 1;
       i += 2;
     }
     else res += 'a' + (s[i] - '1');
