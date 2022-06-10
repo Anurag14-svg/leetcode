8ad2a40cba665fce2a25dcca13a750691d80fefe
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-06-10 09:36:07 -0700

    Time: 104 ms (83.29%), Space: 52.2 MB (34.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,30 +1,30 @@
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         set<int> s;
-//         for(int i = 0 ; i < nums.size() ;i++){
-//             if(s.find(nums[i]) != s.end()){
-//                 return true;
-//             }
-//             s.insert(nums[i]);
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        bool ans = false;
-        
-        for(int i = 0 ; i < nums.size() - 1; i++){
-            if(nums[i] == nums[i+1]){
-                ans = true;
-                break;
+        set<int> s;
+        for(int i = 0 ; i < nums.size() ;i++){
+            if(s.find(nums[i]) != s.end()){
+                return true;
             }
+            s.insert(nums[i]);
         }
-        
-        return ans;
+        return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin() , nums.end());
+//         bool ans = false;
+        
+//         for(int i = 0 ; i < nums.size() - 1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 ans = true;
+//                 break;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
