9bb74d5f293ad924a65144510f9a25d72380eb33
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-06-12 13:49:50 -0700

    Time: 274 ms (5.05%), Space: 148.4 MB (9.60%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,3 +1,4 @@
+// Sorting
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
@@ -7,16 +8,52 @@
 //     }
 // };
 
+
+// Mapping
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int , vector<int> , greater<int>> pq;
+//         for(int i = 0 ; i < nums.size(); i++){
+//             pq.push(nums[i]);
+//             if(pq.size() > k) pq.pop();
+//         }
+        
+        
+//         return pq.top();
+//     }
+// };
+
+
+// Quick Select
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int , vector<int> , greater<int>> pq;
-        for(int i = 0 ; i < nums.size(); i++){
-            pq.push(nums[i]);
-            if(pq.size() > k) pq.pop();
+private:
+    int quickSelect(vector<int> nums , int l , int r, int k){
+        int pivot = nums[r];
+        int p = l;
+        int ind = nums.size() - k;
+        
+        for(int i = l ; i < r ; i++){
+            if(nums[i] < pivot){
+                int temp = nums[i];
+                nums[i] = nums[p];
+                nums[p] = temp;
+                p++;
+            }
         }
         
+        int val = nums[p];
+        nums[p] = nums[r];
+        nums[r] = val;
         
-        return pq.top();
+        if(p < ind) return quickSelect(nums , p+1 , r , k);
+        else if( p > ind) return quickSelect(nums , l , p-1 ,k);
+        else return nums[p];
+    };
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // k = nums.size() - k;
+        return quickSelect(nums , 0 , nums.size()-1 , k);
     }
 };
+
