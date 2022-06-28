7a90d609d17c23ee4dda001f8b562464c26f0837
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-06-28 10:51:02 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (90.75%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-        vector<int> recent;
-        for(int i = 1; i <= numRows; i++){
-            vector<int> temp;
-            if(i == 1){
-                temp.push_back(1);
-                // recent = temp;
+        vector<vector<int>> r(numRows);
+        
+        for(int i = 0; i < numRows; i++){
+            r[i].resize(i+1);
+            r[i][0] = r[i][i] = 1;
+            for(int j = 1; j < i ; j++){
+                r[i][j] = r[i-1][j-1] + r[i-1][j];
             }
-            
-            else if(i == 2){
-                temp.push_back(1);
-                temp.push_back(1);
-                // recent = temp;
-            }
-            
-            else{
-                    temp.push_back(1);
-                    int m = 0;
-                    
-                    
-                    while(m < recent.size() - 1){
-                        temp.push_back(recent[m] + recent[m + 1]);
-                        m++;
-                    }
-                    
-                    
-                    temp.push_back(1);
-                
-            
-            }
-            
-            recent = temp;
-            ans.push_back(temp);
         }
         
-        return ans;
+        
+        return r;
     }
 };
