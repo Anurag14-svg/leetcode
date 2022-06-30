9f97b46f1e00a754ce2ae4620c1afa62199a6064
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-06-30 05:26:13 -0700

    Time: 3 ms (43.51%), Space: 6.5 MB (44.03%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int paths(int i , int j , int m , int n, vector<vector<int>> &dp){
+        if((i == m - 1) && (j == n - 1)) return 1;
+        if((i>=m) || (j >=n)) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = paths(i+1 , j, m , n, dp) + paths(i , j+1, m , n , dp );
+    }
+    
+    int uniquePaths(int m, int n) {
+        int i = 0;
+        int j = 0;
+        
+        vector<vector<int>> dp(m + 1 , vector<int>(n + 1, -1));
+        if( m == 1 && n == 1) return 1;
+        paths(i , j ,m , n, dp);
+        return dp[0][0];
+    }
+};
