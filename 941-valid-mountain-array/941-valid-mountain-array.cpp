6226af9f405c67c47a4daa72f0695a5500578879
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-06 19:14:51 -0700

    Time: 38 ms (66.97%), Space: 22.5 MB (48.14%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         int n = arr.size();
-        int i = 0;
-        while(i+1< n && arr[i] < arr[i+1]){
+        int i = 1;
+        while(i< n && arr[i] > arr[i-1]){
             i++;
         }
         
-        if(i == 0 || i == n-1){
+        if(i == 1 || i == n){
             return false;
         }
         
-        while(i + 1 < n && arr[i] > arr[i+1]){
+        while(i  < n && arr[i] < arr[i-1]){
             i++;
         }
         
-        return i == n - 1;
+        return i == n;
     }
 };
