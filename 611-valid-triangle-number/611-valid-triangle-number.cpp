4716e4d3181957c0ac080db263d6722871d74cc7
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-07 11:58:46 -0700

    Time: 139 ms (72.13%), Space: 13 MB (6.10%) - LeetHub

---
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
+        int ans = 0;
+        
+        for(int i = n - 1; i >= 2 ; i--){
+            int low = 0 , high = i - 1;
+            
+            while(low < high){
+                if(nums[low] + nums[high] > nums[i]){
+                    ans+= (high - low);
+                    high--;
+                }
+                else low++;
+            }
+        }
+        
+        return ans;
+    }
+};
