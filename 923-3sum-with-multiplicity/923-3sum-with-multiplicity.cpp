1fe8ba9830cd8ad541a3c6e4cb0db354447fd392
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-09 14:37:53 -0700

    Time: 19 ms (80.87%), Space: 10.7 MB (45.51%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -7,13 +7,15 @@ public:
         for (auto it : c)
             for (auto it2 : c) {
                 int i = it.first, j = it2.first, k = target - i - j;
-                if (!c.count(k)) continue;
-                if (i == j && j == k)
+                if (c.find(k) != c.end()){
+                    if (i == j && j == k)
                     res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
                 else if (i == j && j != k)
                     res += c[i] * (c[i] - 1) / 2 * c[k];
                 else if (i < j && j < k)
                     res += c[i] * c[j] * c[k];
+                }
+                
             }
         return res % int(1e9 + 7);
     }
