cbe8be0ec5c90fe6625bbd54fe458af18992bdbb
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-12 13:10:55 -0700

    Time: 4 ms (28.40%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,30 +1,48 @@
 class Solution {
-public:
     
-    // int paths(int i , int j , int m , int n, vector<vector<int>> &dp){
-    //     if((i == m - 1) && (j == n - 1)) return 1;
-    //     if((i>=m) || (j >=n)) return 0;
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     else return dp[i][j] = paths(i+1 , j, m , n, dp) + paths(i , j+1, m , n , dp );
-    // }
+public:
+//     Recurrsion
+    
+//     int helper(int m , int n){
+//         if(m == 0 && n == 0) return 1;
+//         if(m < 0 || n < 0) return 0;
+        
+//         int up = helper(m - 1, n);
+//         int right = helper(m , n -1);
+        
+//         return up + right;
+//     }
+    
+//     int uniquePaths(int m, int n) {
+//         // return paths(0 , 0 ,m ,n);
+//         return helper(m - 1, n -1);
+//     }
+    
+    
+int helper(int m , int n, vector<vector<int>> &dp){
+        if(m == 0 && n == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+    
+        if(dp[m][n] != -1) return dp[m][n];
+        
+        int up = helper(m - 1, n, dp);
+        int right = helper(m , n -1, dp);
+        
+        return dp[m][n] = up + right;
+    }
     
     int uniquePaths(int m, int n) {
-//         int i = 0;
-//         int j = 0;
+        // return paths(0 , 0 ,m ,n);
+        vector<vector<int>> dp(m , vector<int> (n , -1));
+        return helper(m - 1, n -1, dp);
+   }
+    
+    
+//     int uniquePaths(int m, int n) {
+//         // return paths(0 , 0 ,m ,n);
+//         // Recurrsion
+//         // return helper(m - 1, n -1);
         
-//         vector<vector<int>> dp(m + 1 , vector<int>(n + 1, -1));
-//         if( m == 1 && n == 1) return 1;
-//         paths(i , j ,m , n, dp);
-//         return dp[0][0];
         
-        int N = (m + n) - 2;
-        int r = m - 1;
-        double res = 1;
-        
-        for(int i = 1; i <= r ;i++){
-            res = res * (N - r + i) / i;
-        }
-        
-        return (int)res;
-    }
+//     }
 };
