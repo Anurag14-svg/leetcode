a1e9cf1c96846cf354fa9a842a78bdb9f9903a09
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-12 14:08:14 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
     
 public:
-//     Recurrsion
+//     Recurrsion -- TLE
     
 //     int helper(int m , int n){
 //         if(m == 0 && n == 0) return 1;
@@ -19,28 +19,54 @@ public:
 //     }
     
     
-int helper(int m , int n, vector<vector<int>> &dp){
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
+// Memoization -- Accepted
     
-        if(dp[m][n] != -1) return dp[m][n];
-        
-        int up = helper(m - 1, n, dp);
-        int right = helper(m , n -1, dp);
-        
-        return dp[m][n] = up + right;
-    }
+// int helper(int m , int n, vector<vector<int>> &dp){
+//         if(m == 0 && n == 0) return 1;
+//         if(m < 0 || n < 0) return 0;
     
-    int uniquePaths(int m, int n) {
-        // return paths(0 , 0 ,m ,n);
-        vector<vector<int>> dp(m , vector<int> (n , -1));
-        return helper(m - 1, n -1, dp);
+//         if(dp[m][n] != -1) return dp[m][n];
+        
+//         int up = helper(m - 1, n, dp);
+//         int right = helper(m , n -1, dp);
+        
+//         return dp[m][n] = up + right;
+//     }
+    
+//     int uniquePaths(int m, int n) {
+//        
+//         vector<vector<int>> dp(m , vector<int> (n , -1));
+//         return helper(m - 1, n -1, dp);
+//    }
+    
+
+// Tabulation
+    
+int uniquePaths(int m, int n) {
+       
+        vector<vector<int>> dp(m , vector<int> (n , 0));
+        for(int i = 0 ;i < m  ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0, right = 0 ;
+                    if(i > 0)
+                         up = dp[i-1][j];
+                    if(j > 0)
+                         right = dp[i][j-1];
+                    
+                    dp[i][j] = up + right;
+                }
+            }
+        }
+    
+    return dp[m-1][n-1];
    }
     
     
 //     int uniquePaths(int m, int n) {
 //         // return paths(0 , 0 ,m ,n);
-//         // Recurrsion
+        
 //         // return helper(m - 1, n -1);
         
         
