1a2c6a73a450fd50bd49a5c04d849cc1e88a7d7e
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-14 11:05:01 -0700

    Time: 6 ms (43.07%), Space: 7.7 MB (75.88%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -23,24 +23,27 @@ public:
         int col = obstacleGrid[0].size();
         
         if(obstacleGrid[0][0] == 1) return 0;
-        vector<vector<int>> dp(row , vector<int>(col , 0));
+        vector<int> dp(col , 0);
         
         for(int i = 0 ; i < row ; i++){
+            vector<int> curr(col , 0);
             for(int j = 0 ;j < col ; j++){
                
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                if(i == 0 && j == 0) curr[j] = 1;
+                else if(obstacleGrid[i][j] == 1) curr[j] = 0;
                 else {
                      int up = 0;
                      int left = 0;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
+                    if(i > 0) up = dp[j];
+                    if(j > 0) left = curr[j-1];
                     
-                    dp[i][j] = up + left;
+                    curr[j] = up + left;
                 }
             }
+            
+            dp = curr;
         }
         
-        return dp[row-1][col-1];
+        return dp[col-1];
     }
 };
