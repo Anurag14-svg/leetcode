5b182a59fb4263e772107dc64c6f6f11c315003a
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-07-22 23:43:01 -0700

    Time: 21 ms (8.71%), Space: 13.7 MB (77.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -12,7 +12,18 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n , - 1);
-        return min(f(n -1 , cost , dp) , f(n -2 , cost , dp));
+        // return min(f(n -1 , cost , dp) , f(n -2 , cost , dp));
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2 ; i < n ; i++){
+            int first = dp[i-1] + cost[i];
+            int second = dp[i-2] + cost[i];
+            
+            dp[i] = min(first , second);
+        }
+        
+        return min(dp[n-1] , dp[n-2]);
         
     }
 };
